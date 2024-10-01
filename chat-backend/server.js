584dc5cf20101f63c4e53042f847cc160eda7080
Nandani <2021uch0021@iitjammu.ac.in>
2024-10-01 16:04:38 +0530

    first commit

---
+++ chat-backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("sendMessage", (message) => {
+    console.log("Received message:", message); // Log the message received
+    io.emit("receiveMessage", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+server.listen(4000, () => {
+  console.log("Server listening on port 4000");
+});
